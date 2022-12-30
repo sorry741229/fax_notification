b7d07cb8e1459dba20775bb0929b65139c3905d3
Martin Chang <sorry741229@gmail.com>
2022-12-30 13:54:07 +0800

    add new function and notificaton window

--- Fax_Notification.py
+++ Fax_Notification.py
@@ -15,6 +15,48 @@ import requests
 from ctypes import windll, byref
 from ctypes.wintypes import SMALL_RECT
 import ctypes
+import threading
+import tkinter as tk
+import pystray
+from PIL import Image
+from pystray import MenuItem, Menu
+
+
+def quit_window(icon: pystray.Icon):
+    icon.stop()
+    win.destroy()
+
+
+def show_window():
+    win.deiconify()
+
+
+def on_exit():
+    win.withdraw()
+
+
+def delete_all():
+    text.delete(1.0, "end")      # 刪除全部內容
+
+
+
+
+menu = (MenuItem('顯示', show_window, default=True), Menu.SEPARATOR, MenuItem('退出', quit_window))
+image = Image.open("light.ico")
+icon = pystray.Icon("icon", image, "Kolink傳真通知", menu)
+win = tk.Tk()
+win.title('Kolink傳真通知紀錄')
+win.geometry("650x130")
+win.resizable(width=False, height=False)
+text = tk.Text(win)  # 顯示文字
+text.config(padx = 1 , pady = 2)
+text.config(bg = '#dcdcdc' , fg = '#191970')
+scrollbar = tk.Scrollbar(win)          # 建立滾動條
+scrollbar.pack(side='right', fill='y')
+btn1=tk.Button(win, text="清空畫面", command=delete_all)
+btn1.pack(side=tk.BOTTOM)
+text.tag_configure("left", justify='left') #輸出文字對齊用
+
 
 WindowsSTDOUT = windll.kernel32.GetStdHandle(-11)
 dimensions = SMALL_RECT(-10, -10, 100, 20) # (left, top, right, bottom)
@@ -28,86 +70,30 @@ def now():
     return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 s = '2023-06-14 23:59:59'
 if now() > s:
-    notification.notify(title = '歡迎使用傳真通知系統', message = 'by Martin Chung at Coolink CNC Dept. in DEC 2022' ,app_icon ='C:/Users/cnc-3/Downloads/light.ico', timeout = 2 )
-
-#if now() > s :
-    # print(Fore.CYAN +"{:=^80s}".format("歡迎使用_公旭群旭眾旭專用_傳真通知"))
-    # print('Welcom to use drawing update detection for Kolink')
-    # print('')
-    # time.sleep(1)
-    # print(Fore.RED +'by Martin Chung at Coolink CNC Dept. in DEC 2022')
-    # time.sleep(3)
-    #notification.notify(title = '歡迎使用傳真通知系統', message = 'by Martin Chung at Coolink CNC Dept. in DEC 2022' ,app_icon ='C:/Users/cnc-3/Downloads/light.ico', timeout = 2 )
-    #os._exit(0)
-    #print('')
-
-# print(Fore.CYAN +"{:=^80s}".format("歡迎使用_公旭群旭眾旭專用_傳真通知"))
-# time.sleep(1)
-# #授權
-# try:
-#     while True:
-#         ans = '5931'
-#         x = 3 #初始機會
-#         while x > 0 :
-#             x = x -1
-#             print('')
-#             pwd = input('請輸入登入密碼: ')
-#             if pwd == ans:
-#                 print('')
-#                 print('')
-#                 break
-#             else:
-#                 print('密碼錯誤!')
-#                 if x > 0:
-#                     print('還有', x,'次機會')
-#                 else:
-#                     print('輸入錯誤三次，程式結束')
-#                     print('')
-#                     print('')
-#                     print('3秒後程式關閉', end = '')
-#                     for i in range(6):
-#                         print("",end = ' ',flush = True)  #flush - 输出是否被缓存通常决定于 file，但如果 flush 关键字参数为 True，流会被强制刷新
-#                         time.sleep(0.5)
-#                         print('')
-#                     os._exit(0)                 
-#         break
-
-#     os.system('cls') #登入後清除畫面
-#     print('')
-#     print(Fore.CYAN +"{:=^80s}".format("歡迎使用_公旭群旭眾旭專用_發行圖面更新通知"))
-#     time.sleep(1)
-#     print('登入成功')
-#     print('')
-# except :
-#         print('Error :something went wrong')
-if os.path.isfile('C:/Users/Public/Documents/傳真通知紀錄.csv'): #檢查檔案在不在--公司用
-    print('找到更新紀錄檔')
+    #notification.notify(title = '歡迎使用Kolink傳真通知', message = 'by F0614 Martin Chung at Coolink CNC Dept. in DEC 2022' ,app_icon ='C:/Users/Public/Pictures/light.ico', timeout = 5 )
+    text.insert(tk.INSERT, 'Welcome to use FAX Notification For Kolink'+ str('\n'))
+    text.insert(tk.INSERT, 'by Martin Chung at Coolink CNC Dept. in DEC 2022'+ str('\n'))
 else:
-    print('更新記錄檔不存在')
-    print('將會自動建立發行圖面更新紀錄檔')
+    text.insert(tk.INSERT, 'Welcome to use FAX Notification For Kolink'+ str('\n'))
+
+if os.path.isfile('C:/Users/Public/Documents/傳真通知紀錄.csv'): #檢查檔案在不在--公司用
+    #print('找到更新紀錄檔')
+    text.insert(tk.INSERT, ' '+str('\n'))
+    text.insert(tk.INSERT, '找到更新紀錄檔...'+str('\n'))
+    text.pack()
+else:
+    #print('更新記錄檔不存在')
+    #print('將會自動建立發行圖面更新紀錄檔')
+    text.insert(tk.INSERT, ' '+str('\n'))
+    text.insert(tk.INSERT, '更新記錄檔不存在'+ str('\n'))
+    text.insert(tk.INSERT, '將會自動建立發行圖面更新紀錄檔...'+ str('\n'))
+    text.pack()
     
     with open('C:/Users/Public/Documents/傳真通知紀錄.csv', 'w', encoding = 'cp950') as f:
         a = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         f.write(str(a)+ '\n')
 
-#line notification
-# url = 'https://notify-api.line.me/api/notify'
-# token = 'yhEteAYjky3GC9YGHIf6ZJ3Iu6MotBieNriyUWRVZ6k'
-# headers = {'Authorization': 'Bearer ' + token}   # 設定權杖
-
-# if os.path.isfile('C:/Users/Public/Documents/傳真通知紀錄.csv'): #檢查檔案在不在--公司用
-#     print('找到更新紀錄檔')
-# else:
-#     print('更新記錄檔不存在')
-#     print('將會自動建立傳真通知紀錄檔')
-
-# print('即將最小化執行...')
-# print('')
-# time.sleep(1.5)
-# print('檔案偵測執行中，請勿關閉視窗')
-# time.sleep(3.5)
-# ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
-notification.notify(title = '傳真通知系統', message = '啟動偵測中' ,app_icon ='C:/Users/cnc-3/Downloads/light.ico', timeout = 1 )#--公司用
+notification.notify(title = 'Kolink傳真通知', message = '啟動偵測中' ,app_icon ='C:/Users/Public/Pictures/light.ico', timeout = 1 )#--公司用
 
 class FileEventHandler(FileSystemEventHandler):
     def __init__(self, aim_path):
@@ -115,14 +101,14 @@ class FileEventHandler(FileSystemEventHandler):
         self.aim_path = aim_path
         self.timer = None
         self.snapshot = DirectorySnapshot(self.aim_path)
-        self.timer = threading.Timer(0.2, self.checkSnapshot)
+        self.timer = threading.Timer(0.5, self.checkSnapshot)
         self.timer.start()
 
     
     def on_any_event(self, event):
-        # if self.timer:
-        #     self.timer.cancel()
-        self.timer = threading.Timer(0.2, self.checkSnapshot)
+        #if self.timer:
+            #self.timer.cancel()
+        self.timer = threading.Timer(0.5, self.checkSnapshot)
         self.timer.start()
     
     def checkSnapshot(self):
@@ -130,16 +116,20 @@ class FileEventHandler(FileSystemEventHandler):
         snapshot = DirectorySnapshot(self.aim_path)
         diff = DirectorySnapshotDiff(self.snapshot, snapshot)
         self.snapshot = snapshot
-        self.timer = threading.Timer(0.2, self.snapshot)
+        self.timer = threading.Timer(0.5, self.snapshot)
         if diff.files_created == []:
             pass
         else:
             for dc in diff.files_created:
                 log = []
-                ans = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))," 傳真-新增: ", dc
-                print(ans[0], Fore.YELLOW + ans[1], Fore.CYAN + ans[2])
-                notification.notify(title = '傳真-新增', message = str(ans[2]) ,app_icon ='C:/Users/cnc-3/Downloads/light.ico', timeout = 2 )
-                
+                ans = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))," 通知: ", dc
+                # 文字標示所在視窗  
+                text.insert(tk.INSERT, str((ans[0][5:]) + ans[1] + ans[2][21:] + '\n'))
+                text.tag_add('left', 1.0, "end")  #輸出文字對齊用
+                text.yview("end") #滾動到最下面
+                text.pack()
+                #print(ans[0], Fore.YELLOW + ans[1], Fore.CYAN + ans[2])
+                notification.notify(title = 'Kolink傳真通知',  message = str((ans[2][21:])) ,app_icon ='C:/Users/Public/Pictures/light.ico', timeout = 1 )
                 #data = {'message':ans}     # 設定要發送的訊息
                 #data = requests.post(url, headers=headers, data=data)   # 使用 POST 方法
                 with open('C:/Users/Public/Documents/傳真通知紀錄.csv', 'r', encoding = 'cp950') as f:
@@ -148,34 +138,22 @@ class FileEventHandler(FileSystemEventHandler):
                 with open('C:/Users/Public/Documents/傳真通知紀錄.csv', 'w', encoding = 'cp950') as f:
                     for l in log:
                         f.write(str(l))
-                    f.write(str(ans[0]) + str(ans[1]) + str(ans[2]) + '\n')
+                    f.write(str(ans[0]) + str(ans[1]) + str((ans[2][15:])) + '\n')
+            
 
-        for dd in diff.files_deleted:
-            log = []
-            ans = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))," 傳真刪除: ", dd
-            print(ans[0], Fore.RED + ans[1], Fore.CYAN + ans[2])
-            notification.notify(title = '傳真-刪除', message = str(ans[2]) ,app_icon ='C:/Users/cnc-3/Downloads/light.ico', timeout = 2 )
-          # print("檔案-修改: ", diff.files_modified)
-            with open('C:/Users/Public/Documents/傳真通知紀錄.csv', 'r', encoding = 'cp950') as f:
-                for txtlog in f:
-                    log.append(str(txtlog))
-            with open('C:/Users/Public/Documents/傳真通知紀錄.csv', 'w', encoding = 'cp950') as f:
-                for l in log:
-                    f.write(str(l))
-                f.write(str(ans[0]) + str(ans[1]) + str(ans[2]) + '\n')           
 
 
 class DirMonitor(object):
-    """文件夹监视类"""
+    """文件夾監視類"""
     
     def __init__(self, aim_path):
-        """构造函数"""
+        """構造函數"""
         
         self.aim_path= aim_path
         self.observer = Observer()
     
     def start(self):
-        """启动"""
+        """啟動"""
         
         event_handler = FileEventHandler(self.aim_path)
         self.observer.schedule(event_handler, self.aim_path, True)
@@ -195,6 +173,13 @@ if __name__ == "__main__":
     monitor.start()
     monitor = DirMonitor(r'//192.168.0.17/共用資料夾/夾層傳真收件夾 2687-3701')
     monitor.start()
+
+    win.protocol('WM_DELETE_WINDOW', on_exit)
+    threading.Thread(target=icon.run, daemon=True).start()
+    scrollbar.config(command=text.yview)
+
+    win.mainloop()
+
     try:
         while True:
             time.sleep(1)
